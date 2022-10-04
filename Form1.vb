5e6502ae36ce4e931c31ca8b15b2d03b99358cf9
justineDDT5A <justinemajau1702@gmail.com>
2022-10-04 10:00:47 +0800

    first commit

---
+++ Form1.vb
@@ -0,0 +1,13 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim months() As String = {"JAN", "FEB", "MAR", "APR", "MAY", "JUN", "JUL", "AUG", "SEP", "OCT", "NOV", "DEC"}
+        For subscript As Integer = 0 To 11
+            ComboBox1.Items.Add(months(subscript))
+        Next subscript
+    End Sub
+End Class
---
+++ LabTask3.vb
@@ -0,0 +1,8 @@
+﻿Public Class LabTask3
+    Private Sub LabTask3_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'Name : Justine Nanggai
+        ' No Matrik : 20DDT20F1010
+        ' Class : DDT5A
+        ' Task :LabTask3
+    End Sub
+End Class
